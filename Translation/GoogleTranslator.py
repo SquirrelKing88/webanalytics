5a0948ffc22aa3d34023e93ff7135ccbaab0436f
igortereshchenko <tereshchenko.igor@gmail.com>
2019-01-22 14:47:13 +0200

    package Translation created: - class GoogleTranslator   get_translation - translate text to a specific language

---
+++ Translation/GoogleTranslator.py
@@ -0,0 +1,25 @@
+from googletrans import Translator
+
+class GoogleTranslator:
+
+    def __init__(self):
+
+        self.__translator = Translator(service_urls=[
+                                                      'translate.google.com',
+
+                                                    ])
+
+    def get_translation(self, original_text, destination_language='en'):
+        """
+
+        :param destination_language: language abbreviation
+        :return: dictionary{'original_language': , 'original_text': , 'translation_language': , 'translation': }
+        """
+
+        translation = self.__translator.translate(original_text, dest=destination_language)
+        return {
+            'original_language': translation.src,
+            'original_text': original_text,
+            'translation_language': destination_language,
+            'translation': translation.text
+        }
---
+++ Translation/test.py
@@ -0,0 +1,9 @@
+
+from Translation.GoogleTranslator import  GoogleTranslator
+
+translator = GoogleTranslator()
+
+result = translator.get_translation("Я люблю програмувати")
+print(result)
+
+
