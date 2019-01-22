d19a9cf7a4afe9909390c2d60d086be0923a27e4
igortereshchenko <tereshchenko.igor@gmail.com>
2019-01-22 09:22:34 +0200

    package Country created: - class CountryHandler   is_country_name - check if country name is correct

---
+++ Country/CountryHandler.py
@@ -0,0 +1,14 @@
+
+class CountryHandler:
+    def __init__(self):
+        # TODO upload all country names
+        self.__countries = dict()
+
+    def is_country_name(self, country_name):
+        """
+
+        :param country_name: name of country
+        :return: boolean
+        """
+        # return country_name in self.__countries
+        return True
