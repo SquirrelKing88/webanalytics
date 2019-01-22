4598f98b446ce636e67bb49faf9384d9eebbdcbc
igortereshchenko <tereshchenko.igor@gmail.com>
2019-01-22 09:21:08 +0200

    package config created:   Google API key added

---
+++ config/__init__.py
@@ -0,0 +1,5 @@
+# lb.team.user@gmail.com
+# password lbteam2018
+# https://developers.google.com/maps/documentation/directions/get-api-key#key
+
+GOOGLE_API_KEY = 'AIzaSyAErPa0emGB3-a_BrcVe-9iNy6r0kpGw-E'
