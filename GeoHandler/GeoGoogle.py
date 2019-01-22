d2732a667bb5e42ede415be8aed47c2ac14d1006
igortereshchenko <tereshchenko.igor@gmail.com>
2019-01-22 09:23:42 +0200

    package GeoHandler created: - class GeoGoogle   get_country_coordinates - get country coordinates by name
      text added
    
    - class GeoOpenStreetMap #TODO

---
+++ GeoHandler/GeoGoogle.py
@@ -0,0 +1,34 @@
+from Country.CountryHandler import CountryHandler
+from config import GOOGLE_API_KEY
+import googlemaps
+
+class GeoGoogle:
+
+    def __init__(self):
+        """
+            Google client initialization with api key from config package
+        """
+
+        self.__gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
+        self.__country_handler = CountryHandler()
+
+
+    def get_country_coordinates(self, country_name):
+        """
+
+        :param country_name: name of country
+        :return: dictionary  {'lat': 48.379433, 'lng': 31.1655799}
+        :raises: Exception if country name not found
+        """
+
+        if not self.__country_handler.is_country_name(country_name):
+            raise Exception("{} is not country name".format(country_name))
+
+        result = self.__gmaps.find_place(country_name, 'textquery')
+
+        # TODO check response status
+        # TODO check data availability
+
+        candidate = result['candidates'][0]['place_id']
+        information = self.__gmaps.place(candidate)
+        return information['result']['geometry']['location']
---
+++ GeoHandler/test.py
@@ -0,0 +1,5 @@
+from GeoHandler.GeoGoogle import GeoGoogle
+
+geo_google = GeoGoogle()
+print(geo_google.get_country_coordinates('Ukraine'))
+
