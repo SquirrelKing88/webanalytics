edafd360fb469a1f43246c945721a6f9b2966c8c
igortereshchenko <tereshchenko.igor@gmail.com>
2019-01-22 16:55:10 +0200

    package Scraper created:   package Writters created     class CommonWriter interface     class FileWriter - writer dictionary to file
      class CommonNewsHandler interface

---
+++ Scraper/CommonNewsHandler.py
@@ -0,0 +1,92 @@
+class CommonNewsHandler:
+    """
+    News handles interface
+    """
+
+    @staticmethod
+    def get_article_row(url=None, date=None, title=None, subtitle=None, html=None, text=None, translation=None):
+        """
+        Create article-dictionary representation
+
+        :param url: article url
+        :param date: publication time
+        :param title: article title
+        :param subtitle: article subtitle
+        :param html: html
+        :param text: cleared text
+        :param translation: english translation
+        :return: dicitonary
+        """
+        return {
+                    "url": url,
+                    "date": date,
+                    "title": title,
+                    "subtitle": subtitle,
+                    "html": html,
+                    "text": text,
+                    "translation_en":translation
+                }
+
+    @staticmethod
+    def parse_articles_list(url_root=None, html=None, soup=None):
+        """
+        Parse list of articles
+
+        :param url_root: server url
+        :param html: html to parse
+        :param soup: instance of Beautiful Soup
+        :return: dictionary{
+                             url_1:{
+                                    "url": url_1,
+                                    "date": date,
+                                    "title": title,
+                                    "subtitle": subtitle,
+                                    "html": None,
+                                    "text": None
+                                    },
+                             url_2:{
+                                    ...
+                                    }
+
+        """
+
+        return None
+
+    @staticmethod
+    def parse_article_time(html=None, soup=None):
+        """
+        Parse article time
+
+        :param html: html to parse
+        :param soup: instance of Beautiful Soup
+        :return: hour, minute, seconds
+        """
+
+        return None, None, None
+
+    @staticmethod
+    def parse_article_subtitle(html=None, soup=None):
+        """
+        Parse article subtitle
+
+        :param html: html to parse
+        :param soup: instance of Beautiful Soup
+        :return: subtitle of article
+        """
+
+        return None
+
+    @staticmethod
+    def parse_article_text(html=None, soup=None):
+        """
+        Parse article text
+
+        :param html: html to parse
+        :param soup: instance of Beautiful Soup
+        :return: html, cleaned html (without tag, text only)
+        """
+
+        return None, None
+
+
+
