dd045b471b1be88e07531093bd210ff9aa2e4f75
MusinDanil <danilsergeyevichmusin@gmail.com>
2019-01-22 11:32:01 +0200

    package Danil - created     file __init__.py updated         function some_function - created

--- team/Danil/__init__.py
+++ team/Danil/__init__.py
@@ -3,5 +3,6 @@ def some_function(some_argument):
     :param some_argument: pass there anything you want
     :return: returns the exact value you passed in
     """
+
     something = some_argument
     return something
