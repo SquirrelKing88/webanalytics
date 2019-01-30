032660d04ef6dc156876f47ba90415b30b070fcc
igortereshchenko <tereshchenko.igor@gmail.com>
2019-01-30 13:20:48 +0200

    test done

--- Requests/test.py
+++ Requests/test.py
@@ -11,5 +11,3 @@ print(Requester.run_html(html))
 requester = Requester(url="http://vk.com", proxy="87.181.249.204")
 response = requester.make_get_request()
 print(response.data.decode('utf-8'))
-
-asdas
