e489b904be12628f6741ae3592f7483d10124b34
igortereshchenko <tereshchenko.igor@gmail.com>
2019-01-30 08:17:34 +0200

    branch scraper

--- GeoHandler/test.py
+++
@@ -1,5 +0,0 @@
-from GeoHandler.GeoGoogle import GeoGoogle
-
-geo_google = GeoGoogle()
-print(geo_google.get_country_coordinates('Ukraine'))
-
--- LanguageProcessing/Translation/test.py
+++
@@ -1,9 +0,0 @@
-
-from LanguageProcessing.Translation import  GoogleTranslator
-
-translator = GoogleTranslator()
-
-result = translator.get_translation("Я люблю програмувати")
-print(result)
-
-
