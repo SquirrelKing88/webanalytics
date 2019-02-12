ab719002005657bb46df2a258b874b2c7d55261e
igortereshchenko <tereshchenko.igor@gmail.com>
2019-02-12 17:05:15 +0200

    added PolyglotAnalysis.py

--- LanguageProcessing/TextAnalysis/PolyglotAnalysis/PolyglotAnalysis.py
+++ LanguageProcessing/TextAnalysis/PolyglotAnalysis/PolyglotAnalysis.py
@@ -1,50 +1,47 @@
-import polyglot
-
-
-# from polyglot.detect import Detector
-#
-# arabic_text = u"""
-# Слава україні
-# """
-# detector = Detector(arabic_text)
-# print(detector.language.code)
-
-
-
-
-
 import os
-import sys
-import polyglot
-dir_path = os.path.dirname(os.path.realpath(__file__))
-path=os.path.join(dir_path, 'data')
-polyglot.polyglot_path = path
 
+import polyglot
 from polyglot.text import Text
 from polyglot.downloader import downloader
+from polyglot.detect import Detector
+from LanguageProcessing.LanguageHandler import LanguageHandler
 
 
 
 
 
 
+class PolyglotAnalysis:
+
+  def __init__(self, text):
+
+    self.__text = text
+
+    detector = Detector(text)
+    self.__language_abbreviation = detector.language.code
 
 
-# downloader.download("ner2.uk")
-#
-#
-# downloader.list(show_packages=False)
+    model_path = os.path.join(polyglot.polyglot_path, 'ner2', self.__language_abbreviation)
 
-blob = u"""
-Народні депутати фракції "Опозиційного блоку" Нестор Шуфрич, Вадим Рабінович і Василь Німченко просять Генпрокуратуру і Державне бюро розслідувань розслідувати "підготовку замаху" на кума президента Росії Віктора Медведчука. Депутатські звернення до директора ДБР Романа Труби і генпрокурора Юрія Луценка оприлюднила прес-служба партії "Опозиційна платформа - За життя".
-Нардепи зазначають, що спираються на інформацію про підготовку замаху, яку оприлюднив екс-президент України Віктор Янукович на прес-конференції 6 лютого 2019 року.
-"""
-text = Text(blob)
+    if not os.path.isdir(model_path):
+
+      # upload model
+      for value in LanguageHandler.languages.values():
+        downloader.download("ner2.{0}".format(value))
+        downloader.download("embeddings2.{0}".format(value))
 
 
-for sent in text.sentences:
-  print(sent, "\n")
-  for entity in sent.entities:
-    print(entity.tag, entity)
+    #Entity Location Persons
+
+    text = Text(text)
+
+
+    for sentence in text.sentences:
+      for entity in sentence.entities:
+        print(entity.tag, entity)
+
+
+
+
 
 
