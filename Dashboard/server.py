82cc0cd4f2119f967fc7732879bceb97a96c1bfe
Admin <chorenko12345@gmail.com>
2019-02-14 10:37:29 +0200

    function for 'choropleth' added(not working)

--- Dashboard/server.py
+++ Dashboard/server.py
@@ -2,7 +2,7 @@ import plotly
 import plotly.graph_objs as go
 import numpy as np
 import json
-
+import pandas as pd
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -30,39 +30,43 @@ def update_world():
 
 @app.route('/', methods=['GET'])
 def index():
-    x = np.linspace(-np.pi, np.pi, 50)
-    y = np.sin(x)
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
 
-    line = go.Scatter(
-        x=x,
-        y=y,
-        name="sin(x)"
+    data = [dict(
+        type='choropleth',
+        locations=df['CODE'],
+        z=df['GDP (BILLIONS)'],
+        text=df['COUNTRY'],
+        colorscale=[[0, "rgb(5, 10, 172)"], [0.35, "rgb(40, 60, 190)"], [0.5, "rgb(70, 100, 245)"], \
+                    [0.6, "rgb(90, 120, 245)"], [0.7, "rgb(106, 137, 247)"], [1, "rgb(220, 220, 220)"]],
+        autocolorscale=False,
+        reversescale=True,
+        marker=dict(
+            line=dict(
+                color='rgb(180,180,180)',
+                width=0.5
+            )),
+        colorbar=dict(
+            autotick=False,
+            title='test'),
+    )]
+
+    layout = dict(
+
+        geo=dict(
+            showframe=False,
+            showcoastlines=False,
+            projection=dict(
+                type='Mercator'
+            )
+        )
     )
 
-    bar = go.Bar(
-        x=["House", "Car"],
-        y=[100, 300]
-    )
+    fig = dict(data=data, layout=layout)
 
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
-
-
-    data = {
-            'line_id':{
-                        'data':[line],
-                        'layout':dict(title='first graph')
-                     },
-
-            'bar_id':  {
-                        'data':[bar],
-                        'layout': dict(title='second graph')
-                    }
-
-            }
-    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
-
-
-
+    plotly.offline.plot(fig, validate=False, filename='d3-world-map.html')
     return render_template('graphs.html', graphJSON=graphJSON)
 
 
