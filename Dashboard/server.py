d032dfef3d63bac22dd7435c21abc17fa3bb5055
igortereshchenko <tereshchenko.igor@gmail.com>
2019-02-15 22:12:30 +0200

    map and pie

--- Dashboard/server.py
+++ Dashboard/server.py
@@ -11,109 +11,100 @@ app.secret_key = 'development key'
 # https://www.kaggle.com/shep312/plotlycountrycodes
 # TODO add other countries
 world = {
-    'Afghanistan': {
-        'code': 'AFG',
-        'data': {
-            'value': 12
-        }
-    },
-    'Albania': {
-        'code': 'ALB',
-        'data': {
-            'value': 100
-        }
-    },
-    'Algeria': {
-        'code': 'DZA',
-        'data': {
-            'value': 300
-        }
-    },
     'Ukraine': {
         'code': 'UKR',
-        'data': {
-            'value': 3003
-        }
-    },
+        'coordinates':{
+                        'latitude':50.45466,
+                        'longitude':30.5238
+                      },
+        'information': {
+                        # all in English
+                        'persons':{'bob','boba'},
+
+                        'organizations':{'org1', 'org2'},
+
+                        'locations':{'location 1','location2'}
+                        },
+                },
+
+    'Poland':{
+        'code': None,
+        'coordinates':{
+                        'latitude':52.237049,
+                        'longitude': 21.017532
+                      },
+        'information': {
+                        # all in English
+                        'persons':{'bob pl','boba pl'},
+
+                        'organizations':{'org2', 'org2'},
+
+                        'locations':{'location 4','location5'}
+
+                       },
+    }
 }
-
-
-# http://127.0.0.1:5000/world?country=Ukraine&value=126
-
-@app.route('/world', methods=['GET'])
-def update_world():
-    country = request.args.get('country')
-    value = request.args.get('value')
-
-    # TODO if value not itn nothing change
-    if country in world:
-        world[country]['data']['value']= int(value)
-
-    return json.dumps(world)
-
-
 
 
 
 @app.route('/', methods=['GET'])
 def index():
-    world_data = pd.DataFrame.from_dict(world)
 
-    data = [dict(
-        type='choropleth',
-
-        locations=list(world_data.loc['code',:]),
-        # TODO text=...,
-        z=[ data['value'] for data in list(world_data.loc['data',:]) ],
-
-        colorscale=[[0, "rgb(5, 10, 172)"], [0.35, "rgb(40, 60, 190)"], [0.5, "rgb(70, 100, 245)"], \
-                    [0.6, "rgb(90, 120, 245)"], [0.7, "rgb(106, 137, 247)"], [1, "rgb(220, 220, 220)"]],
-        autocolorscale=False,
-        reversescale=True,
-        marker=dict(
-            line=dict(
-                color='rgb(180,180,180)',
-                width=0.5
-            )),
-        colorbar=dict(
-            autotick=False,
-            title='test'),
-    )]
-
-    layout = dict(
-
-        autosize=False,
-        width=1000,
-        height=1000,
-
-        geo=dict(
-            showframe=False,
-            showcoastlines=False,
-            projection=dict(
-                type='Mercator'
-            )
-        )
-    )
-
-    fig = {'map': dict(data=data, layout=layout)}
-
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    # TODO somehow create this dataset
 
 
-    return render_template('graphs.html', graphJSON=graphJSON)
+    pie_data = [
+        {
+            "title": "Ukraine",
+            "latitude": 50.45466,
+            "longitude": 30.5238,
+            "width": 50,
+            "height": 50,
+            "pieData": [
+                            {
+                                "category": "bob",
+                                "value": 10
+                            },
+                            {
+                                "category": "boba",
+                                "value": 30
+                            },
+                            {
+                                "category": "boban",
+                                "value": 100
+                            }
+                        ]
+        },
 
+        {
+            "title": "Poland",
+            "latitude": 52.237049,
+            "longitude": 21.017532,
+            "width": 50,
+            "height": 50,
+            "pieData": [
+                {
+                    "category": "Category #1",
+                    "value": 200
+                },
+                {
+                    "category": "Category #2",
+                    "value": 300
+                }
+            ]
+        },
 
-
-@app.route('/google_graph', methods=['GET'])
-def google_graph():
-
-    world_data = [
-        ['Country', 'Popularity'],
     ]
 
-    world_data.extend( [ [country, world[country]['data']['value'] ] for country in world] )
+    data_json = json.dumps(pie_data)
 
-    return render_template('google_graphs.html', world_data=world_data)
+
+    return render_template('index.html', data_json=data_json)
+
+
+
+
+
 
 
 if __name__ == '__main__':
