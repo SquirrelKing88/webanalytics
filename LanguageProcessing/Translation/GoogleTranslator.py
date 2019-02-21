dce4ab977fd291eef493591e2901a9a5e66511fb
SashkoArtemuk <vovkkostrovich@gmail.com>
2019-02-21 11:10:36 +0200

    2500-5000 translator

--- LanguageProcessing/Translation/GoogleTranslator.py
+++ LanguageProcessing/Translation/GoogleTranslator.py
@@ -1,5 +1,6 @@
 import json
 import time
+import re
 
 from bs4 import BeautifulSoup
 from googletrans import Translator
@@ -17,7 +18,7 @@ class GoogleTranslator:
         self.__browser.make_get_request('https://translate.google.com.ua/?tl={0}'.format(destination_language))
 
 
-    def get_translation(self, original_text, max_sentence_count=10):
+    def get_translation(self, original_text, max_symbols_count=2500):
         """
         :param original_text: text to translate
         :param destination_language: language abbreviation
@@ -33,10 +34,49 @@ class GoogleTranslator:
                         'translation': None
                     }
 
-        try:
-            self.__browser.set_element_text('source', json.dumps(original_text))
-            self.__browser.push_element(parent_tag='div', parent_class='go-button', element_tag='div',
-                                 element_class='jfk-button-img')
+        original_text = original_text.replace('\n',' ')
+        sentences = original_text.split('\n')
+
+        formated_text=[]
+
+        words = 0
+        temp = ''
+        for sentence in sentences:
+            words += len(sentence)
+            temp+=sentence
+            if words > 2500:
+                if words >4900:
+                    temp = ''
+                    words = 0
+                    new_sentences=sentence.split('.')
+                    for new_sentence in new_sentences:
+                        new_sentence+='.'
+                        words += len(new_sentence)
+                        temp += new_sentence
+                        if words > 2500:
+                            #print(words)
+                            formated_text.append(temp)
+                            words = 0
+                            temp = ''
+                #print(words)
+                formated_text.append(temp)
+                words = 0
+                temp = ''
+
+        if temp:
+            formated_text.append(temp)
+
+        #print(len(formated_text))
+
+        result = []
+
+        original_language = None
+
+        for text in formated_text:
+            #try:
+            text=text.replace('\n','')
+            self.__browser.set_element_text('source', json.dumps(text))
+            #self.__browser.push_element(parent_tag='div', parent_class='go-button', element_tag='div',element_class='jfk-button-img')
 
             time.sleep(self.__timeout)
 
@@ -46,8 +86,28 @@ class GoogleTranslator:
 
             translation = translation_html.text
 
-        except Exception:
-            translation = None
+            if not original_language:
+                language = soup.find('div', {'role': 'button', 'value':'auto'}).text
+                #print(language)
+                language = re.split(' ',language)[2]
+                language = language.replace('\n', '')
+                self.__browser.set_element_text('source', json.dumps(language))
+                # self.__browser.push_element(parent_tag='div', parent_class='go-button', element_tag='div',element_class='jfk-button-img')
+
+                time.sleep(self.__timeout)
+
+                html = self.__browser.get_html()
+                soup = BeautifulSoup(html, 'html.parser')
+                original_language = soup.find('span', {'class': ['tlid-translation', 'translation']}).text.lower()
+
+
+            result.append(translation)
+            #print(translation)
+
+            #except Exception:
+            #    translation = None
+
+        #print(result)
 
         # # TODO if not detected
         # detected = self.__translator.detect(original_text[:100])
@@ -69,10 +129,12 @@ class GoogleTranslator:
         #     translation_result = self.__translator.translate(" ".join(text_batch), dest=destination_language)
         #     translation += translation_result.text
 
-
+        translation = ''
+        for text in result:
+            translation += text
 
         return {
-            'original_language': '#TODO',
+            'original_language': original_language,
             'original_text': original_text,
             'translation_language': self.__destination_language,
             'translation': translation
