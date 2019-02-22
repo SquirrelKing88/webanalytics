05405623a1e410c47335c4df67ecb1e5d608fb10
igortereshchenko <tereshchenko.igor@gmail.com>
2019-02-22 12:06:45 +0200

    language detection added block translation added

--- LanguageProcessing/Translation/GoogleTranslator.py
+++ LanguageProcessing/Translation/GoogleTranslator.py
@@ -1,11 +1,11 @@
 import json
 import time
-import re
 
 from bs4 import BeautifulSoup
 from LanguageProcessing.LanguageHandler import LanguageHandler
 from Requests.WebBrowser.SeleniumBrowser import SeleniumBrowser
 
+from polyglot.detect import Detector
 
 class GoogleTranslator:
 
@@ -34,35 +34,30 @@ class GoogleTranslator:
         if not original_text:
             return result
 
-
-        # TODO detected language
-        paragraph = original_text[:100]
-        original_language =''
-        result['original_language'] = original_language
-
+        detector = Detector(original_text[:100])
+        result['original_language'] = detector.language.code
 
         if len(original_text) > max_symbols_count:
-            tokenizer = LanguageHandler.get_tokenizer(original_language)
+            tokenizer = LanguageHandler.get_tokenizer(result['original_language'])
             sentences = tokenizer.tokenize(original_text.strip())
         else:
             sentences = [original_text]
 
+        sentences.reverse()
+        result['translation'] = ''
         while sentences:
             block = ''
-            while len(block) < max_symbols_count:
-                block += sentences.pop()
+            while len(block) < max_symbols_count and sentences:
+                block += sentences.pop()+' '
 
-            block_translation = self.__get_translation(block)
+            block_translation = self.__get_translation(block,result['original_language'])
 
-            if not (result['translation']):
-                result['translation'] = block_translation['translation']
-            else:
-                result['translation'] += block_translation['translation']
+            result['translation'] += block_translation['translation']
 
         return result
 
 
-    def __get_translation(self, original_text):
+    def __get_translation(self, original_text, original_language):
         """
         :param original_text: text to translate
         :return: dictionary{'original_language': , 'original_text': , 'translation_language': , 'translation': }
@@ -76,29 +71,35 @@ class GoogleTranslator:
                         'translation': None
                     }
 
-        translation = ''
-        counter = 1
-        while not translation and counter <= self.__timeout:
+        self.__browser.set_element_text('source', json.dumps(' '))
 
-            self.__browser.set_element_text('source', json.dumps(original_text))
-            time.sleep(1)
-
+        # wait until clean input div
+        while True:
             html = self.__browser.get_html()
             soup = BeautifulSoup(html, 'html.parser')
             translation_html = soup.find('span', {'class': ['tlid-translation', 'translation']})
-            translation = translation_html.text
 
-            counter += 1
+            if not translation_html:
+                break
 
-        # TODO check for arabic language
-        language = soup.find('div', {'role': 'button', 'value' : 'auto'}).text
-        language = re.split(' ', language)[2]
-        original_language = language.replace('\n', '')
+            self.__browser.push_element(parent_tag='div', parent_class='clear-wrap', element_tag='div', element_class='jfk-button-img')
 
-        # TODO if not detected
-        if not len(translation):
-            translation = None
-            original_language = None
+        translation = ''
+        begin = time.monotonic()
+        end = time.monotonic()
+        self.__browser.set_element_text('source', json.dumps(original_text))
+
+        while not translation and (end-begin) <= self.__timeout:
+
+            end = time.monotonic()
+            html = self.__browser.get_html()
+            soup = BeautifulSoup(html, 'html.parser')
+            translation_html = soup.find('span', {'class': ['tlid-translation', 'translation']})
+            if translation_html:
+                translation = translation_html.text
+
+        # clear browser
+        self.__browser.set_element_text('source', json.dumps(' '))
 
         return {
                     'original_language': original_language,
--- LanguageProcessing/Translation/test.py
+++ LanguageProcessing/Translation/test.py
@@ -1,8 +1,9 @@
 
 from LanguageProcessing.Translation.GoogleTranslator import  GoogleTranslator
 
-translator = GoogleTranslator(destination_language='uk')
 
+
+translator = GoogleTranslator(destination_language='uk')
 result = translator.get_translation(u"""
 In the west, M-28 begins at a signalized intersection with US 2 in Wakefield. Heading north, the highway passes Sunday Lake heading out of town. After crossing into southwestern Ontonagon County and the Eastern Time Zone, the trunkline highway skirts the northern shore of Lake Gogebic, running concurrently with M-64. The first section of M-28 designated as a part of the Lake Superior Circle Tour is from the western terminus to the eastern junction with M-64 in Bergland, where the Circle Tour turns north along M-64, leaving M-28. Here, M-28 has its lowest traffic counts; within the 2013 MDOT survey, the road is listed with only an average annual daily traffic (AADT) of 1,425 vehicles on a section of highway between Bergland and the US 45 intersection in Bruce Crossing.[6] The trunkline runs through heavily forested areas of southern Houghton and Baraga counties. At the eastern junction with US 41 near Covington, M-28 receives the Circle Tour designation again[4] and exits the Ottawa National Forest.[7]
 
@@ -13,7 +14,17 @@ At the Ishpeming–Negaunee city line, M-28 changes memorial highway designation
 A brown and white wooden sign for the Hiawatha National Forest mounted on a stone base. The sign is installed on the right side of a highway between the road and the forest on the far right.
 Hiawatha National Forest road sign on M-28/M-94 in Alger County west of Shingleton
 Between Marquette and Munising, M-28 closely parallels the Lake Superior shoreline, providing scenic views of the lake and its "lonesome sandy beaches".[11][12] The Lakenenland Sculpture Park is located in Chocolay Township near Shot Point in eastern Marquette County. This roadside attraction is owned by Tom Lakenen and features fanciful works of art made of scrap iron.[13] Near the community of Au Train, M-28 crosses into the western unit of the Hiawatha National Forest.[7] West of Munising is a ferry dock offering transport to the Grand Island National Recreation Area, and at Munising there is easy access to Pictured Rocks National Lakeshore. The roadway also features variable-message signs to warn motorists of winter weather-related traffic closures along the lakeshore. Installed at the US 41 and M-94 junctions, the signs advise motorists which sections of roadway are closed. Per MDOT policy, only snowplows are allowed on these sections during a closure.[14] The highway exits the Hiawatha National Forest at the Alger County–Schoolcraft County line along the Seney Stretch.
-""", max_symbols_count=50)
-print(result)
+""", max_symbols_count=4000)
+print(result['translation'])
+
+
+print('\n\n\n')
+
+translator = GoogleTranslator()
+result = translator.get_translation(u"""
+للغه العربى (اللغة العربية) هى لغه ساميه مركزيه, و بكده تصنيفها بيكون مع اللغات الساميه التانيه زى العبرى والأرامى. العربى القياسى الحديث هو اللى بيتدرس فى المدارس والجامعات, و بيستعمل فى اماكن الشغل و الحكومه و معظم وسايل الإعلام (ميديا).[1] كود اللغه بالنسبه لقانون المنظمه الدوليه للتوحيد القياسى (ايزو 639) (ISO 639), هو ar
+اللهجة السايده فى كل بلد لغته الرسمى عربى مختلفه. مثلا فى مصر مع ان اللغه الرسمى عربى, معظم سكّانها بيتكلّمو اللغه المصريه الحديثه,[1] و هى لغه معترف بيها فى العالم عن طريق كود اللغه بالنسبه لقانون المنظمه الدوليه للتوحيد القياسى (ايزو 639) (ISO 639), و هو arz. فيه مصادر بتقول ان انواع اللغه العربى (انجليزى: varieties of Arabic; مش اللغه العربى الكلاسيكى او القياسيه الحديثه) بيستعملها 206 مليون شخص كلغتهم الاولانيه و 248 مليون شخص غيرهم
+""", max_symbols_count=4000)
+print(result['translation'])
 
 
